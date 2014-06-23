5911671809efd8986851a84682cd382e5b4fc3ae
Henry Huang <hhuang2088@gmail.com>
2014-06-23 09:46:01 -0700

    app now centers google map at user's open location

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,20 +1,28 @@
 class SpotsController < ApplicationController
   before_action :authenticate_user!
 
+  def index 
+    @spots = current_user.spots.all 
+
+    respond_to do |f|
+      f.json { render :json => @spots}
+    end
+  end
+
   def new 
-    @spot = Spot.new
+    @spot = current_user.spots.new
   end
 
   def create 
-    @spot = Spot.new(spot_params)
+    @spot = current_user.spots.new(spot_params)
   end
 
   def show 
-    @spot = Spot.find(params[:id])
+    @spot = current_user.spots.find(params[:id])
   end
 
   def delete 
-    @spot = Spot.find(params[:id])
+    @spot = current_user.spots.find(params[:id])
   end
 
   private 
